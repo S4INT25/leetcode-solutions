73a5b5d51d7e29c6b54f294c8ff0611289f28e58
S4INT <lucksomwanambulo25@gmail.com>
2022-01-31 06:03:23 +0200

    Richest Customer Wealth

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,32 @@
-import solutions.easy.palindromeNumber
+import solutions.easy.maximumWealth
+import solutions.easy.maximumWealth2
+import kotlin.concurrent.timer
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
+
 
 fun main() {
-    print(palindromeNumber(23))
+    val test1 = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 1, 2, 3),
+        intArrayOf(3, 2, 1, 5, 4),
+        intArrayOf(3, 3, 3, 3, 3),
+        intArrayOf(2, 1, 2, 1, 2),
+        intArrayOf(1, 2, 3, 4, 5),
+        intArrayOf(1, 2, 3, 4, 5),
+        intArrayOf(1, 2, 3, 4, 5),
+    )
+
+    val timeTaken2 = measureTimeMillis {
+        maximumWealth2(test1)
+    }
+
+    val timeTaken = measureTimeMillis {
+        maximumWealth(test1)
+    }
+
+
+    println(" Time1 taken: $timeTaken ms")
+    println(" Time2 taken: $timeTaken2 ms")
+
 }
--- src/main/kotlin/solutions/easy/SumOfTwo.kt
+++ src/main/kotlin/solutions/easy/SumOfTwo.kt
@@ -1,5 +1,7 @@
 package solutions.easy
 
+import java.util.*
+
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
